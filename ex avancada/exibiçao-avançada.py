85516f23bd228ad0f5f01e494b1d7a022810634e
Christian Soares <chrisfilho19@gmail.com>
2015-07-13 16:16:04 -0300

    exibiçao avancada

---
+++ ex avancada/exibiçao-avançada.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+string_1 = "inlglaterra"
+string_2 = "pais"
+string_3 = "sim"
+string_4 = "não"
+print "Nao vamos para %s. E um %s bobo.vamos %s.nao vamos %s" % (string_1, string_2, string_3, string_4)
+
+
+name = raw_input("Qual e o seu nome?")
+quest = raw_input("Qual e sua missao?")
+color = raw_input("Qual e sua cor favorita?")
+emprego = raw_input("Qual e seu emprogo ou funçao?")
+name = "mike"
+age = "19"
+pais = "brazil"
+emprego = presidente
+print "Ah, entao seu nome e %s, sua missao e %s , " \
+    "e sua cor favorita e %s ." % (name, quest, color, pais)
+, pais)
