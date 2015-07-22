acd2b00c50e99a9b0fca4e42521d83800edaeb3f
Christian Soares <chrisfilho19@gmail.com>
2015-07-22 20:15:09 -0300

    não e preciso colocar o nome das operacoẽs

---
+++ matematica.py/mat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print "No python e normal usar soma, subtracao, multiplicacao,divisao e exponenciacao. "
+print "vamos ao exemplo:"
+
+
+Adicao = 19920 + 23
+Subtracao = 111 - 111
+Multiplicacao = 333 * 333
+Divisao = 32 / 16
+Exponenciacao = 20 ** 2
+
+print "agora vamos exibir. "
+
+print Adicao
+print Subtracao
+print Multiplicacao
+print Divisao
+print Exponenciacao
