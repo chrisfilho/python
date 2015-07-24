4ca44a38d74adc7be33064bf6dff74eb4c983bc9
Christian Soares <chrisfilho19@gmail.com>
2015-07-24 18:38:06 -0300

    comparadores

---
+++ comp.py/comparadores.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print "Existem seis omparadores que verificam se um valor é (ou não e) igual, maior do que (ou indiferente ) outro valor."
+
+
+print 'Igual a'("==")
+print 'Diferente de'("!=")
+print 'Menor do que'("<")
+print 'Menor ou indiferente a'("<=")
+print 'Maior que'(">")
+print 'Maior ou indiferente a'(">=")
+
+
+print'agora um exemplo'
+
+print "menor" = 17 < 328
+
+
+print "Igual" = 1000 == (4 * 250)
+
+
+print "Menor ou indiferente	" = 19 <= 28
+
+
+print "Maior ou indiferente" = - 1000 >= -999
+
+print "Diferente" = 999 != (998 + 1)
+
+
+print "maior" = 999 != (2 > 1)
