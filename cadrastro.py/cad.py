1176c530784555ec3c9e3c4c6306bffaa35339f1
Christian Soares <chrisfilho19@gmail.com>
2015-07-25 14:38:22 -0300

    este e um cadrastrador para sua pagina

---
+++ cadrastro.py/cad.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print 'bem vindo ao Facebok'
+
+nome = raw_input("insira o seu nome completo: ")
+
+
+senha = raw_input("agora sua senha:")
+
+data = raw_input("por ultimo sua data de nascimento:")
+
+print "parabens agora voce e cadastrado no Facebok"
+print "aproveite nossos servicos"
+print "seu nome: %s" % nome
+
+print "sua senha: %s" % senha
+
+print "sua data: %s" % data
