bbdf455ea2bbef71c2fb5ff31f0cdf362b00de21
Christian Soares <chrisfilho19@gmail.com>
2015-07-26 23:03:02 -0300

    Renomendo pasta

---
+++ comp/comparadores.py
@@ -0,0 +1,14 @@
+print 'Facebok'
+
+b = raw_input("insira o seu nome: ")
+
+
+a = raw_input("agora sua senha")
+
+print parabens agora voce e cadastrado no Facebok
+print a
+print b
+
+
+else:
+    "desculpe mas um dos espacos estao vazios"
